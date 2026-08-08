9b9bdce8ba694987651978ad0e1f4461182ed81d
ASIUS516 <azizmemmedbeyov@gmail.com>
2026-08-09 00:07:59 +0400

    Update About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -48,27 +48,3 @@ export default function About({ text, enabled = true }) {
     </section>
   );
 }
-
-  useEffect(() => {
-    if (!started) return;
-    setShown("");
-    let i = 0;
-    const speed = 70;
-    const interval = setInterval(() => {
-      i++;
-      setShown(text.slice(0, i));
-      if (i >= text.length) clearInterval(interval);
-    }, speed);
-    return () => clearInterval(interval);
-  }, [started, text]);
-
-  return (
-    <section id="about" className="about" ref={ref}>
-      <span className="about__eyebrow">{t.about}</span>
-      <p className="about__text">
-        {shown}
-        <span className="about__cursor" />
-      </p>
-    </section>
-  );
-}
